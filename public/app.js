e5e271111c81ac2eed802d635a21f2f2ff63b84e
akmatkulov <akmatkulovs991@gmail.com>
2023-08-11 23:54:12 +0600

    Pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -2,6 +2,9 @@ function something() {
     var x = window.localStorage.getItem('bbb');
     x = x * 1 + 1;
     window.localStorage.setItem('bbb', x);
-    alert(x);
-    
+    alert(x);  
+}
+
+function add_to_cart(id) {
+    alert('You added pizza id: ' + id);
 }
